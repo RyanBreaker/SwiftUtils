190030573d0746fc574b5d19bba502a59715acf7
Ryan Breaker <ryan@breaker.rocks>
2016-01-21 19:53:20 -0600

    Initial Commit

---
+++ Option.swift
@@ -0,0 +1,12 @@
+//
+//  Option.swift
+//  SwiftUtils
+//
+//  Created by Ryan Breaker on 1/19/16.
+//  Copyright © 2016 Ryan Breaker. All rights reserved.
+//
+
+import Foundation
+import OptionKit
+
+
