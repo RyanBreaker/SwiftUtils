eba076bc55d2437269ae44391683cba1d2e592ce
Ryan Breaker <ryan@breaker.rocks>
2016-01-26 16:19:02 -0600

    More work on ls

--- ls/main.swift
+++ ls/main.swift
@@ -10,21 +10,60 @@ import Foundation
 import OptionKit
 
 
+
+internal struct FileAttributes {
+	let fileName: String
+	let fileSize: UInt64
+
+	init(_ fileName: String, _ fileAttributes: [String: AnyObject]?) throws {
+		guard let fileAttributesUnwrapped = fileAttributes
+			else {
+				throw FileAttributesError.NilFileAttributes
+		}
+
+		guard let fileSizeAnyObject = fileAttributesUnwrapped["NSFileSize"]
+			else {
+				throw FileAttributesError.DictionaryAccessFailed(key: "NSFileSize")
+		}
+
+		guard let fileSize = fileSizeAnyObject as? UInt64
+			else {
+				throw FileAttributesError.FailedDowncast
+		}
+
+		self.fileName = fileName
+		self.fileSize = fileSize
+	}
+}
+
+enum FileAttributesError: ErrorType {
+	case NilFileAttributes
+	case DictionaryAccessFailed(key: String)
+	case FailedDowncast
+}
+
+
+
 let fManager = NSFileManager.defaultManager()
 
 let cwd = fManager.currentDirectoryPath
-let cwdContents = (try! fManager.contentsOfDirectoryAtPath(cwd))
-	.sort( { s1, s2 in return s1 < s2 } )  // Sort alphabetically
+let cwdContents: [String]
+
+do {
+	cwdContents = try fManager.contentsOfDirectoryAtPath(cwd)
+		.sort( { s1, s2 in return s1 < s2 } )
+} catch let error as NSError {
+	print(error.localizedDescription)
+	exit(1)
+}
 
 
 var outString = ""
 
 for file in cwdContents {
-	let attributes = try! fManager.attributesOfItemAtPath(file)
-	let fileSize = attributes["NSFileSize"] as! Int
-	
-	outString += "\(fileSize)\t\(file)\n"
 }
 
 
 print(outString, terminator: "")
+
+
