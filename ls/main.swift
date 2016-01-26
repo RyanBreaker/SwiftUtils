2ec00f773bb7e2bfdeccfc80820311e5033b885d
Ryan Breaker <ryan@breaker.rocks>
2016-01-25 22:55:21 -0600

    Additions to ls

--- ls/main.swift
+++ ls/main.swift
@@ -12,5 +12,19 @@ import OptionKit
 
 let fManager = NSFileManager.defaultManager()
 
-print(fManager.currentDirectoryPath)
-print(try! fManager.contentsOfDirectoryAtPath(fManager.currentDirectoryPath))
+let cwd = fManager.currentDirectoryPath
+let cwdContents = (try! fManager.contentsOfDirectoryAtPath(cwd))
+	.sort( { s1, s2 in return s1 < s2 } )  // Sort alphabetically
+
+
+var outString = ""
+
+for file in cwdContents {
+	let attributes = try! fManager.attributesOfItemAtPath(file)
+	let fileSize = attributes["NSFileSize"] as! Int
+	
+	outString += "\(fileSize)\t\(file)\n"
+}
+
+
+print(outString, terminator: "")
